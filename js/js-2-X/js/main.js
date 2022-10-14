4e4d6e314d8fe1590bbb941e2b278df0181527f2
miltondalelandrade <robertnbritton@gmail.com>
2022-10-14 09:13:34 +0200

    Lesson #5.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,29 +1,14 @@
-const myNumber = 42
-
-console.log(myNumber)
-
-const myFloat = 42.0
-
-const myString = "42"
-
-console.log(myFloat)
-
-console.log(myFloat === myNumber)
-console.log(myString === myNumber)
-console.log(myString === myFloat)
-console.log(Number(myString) === myFloat)
-console.log(Number(myString) === myNumber)
-console.log(Number("Andrej"))
-console.log(Number(true))
-console.log(Number(false))
-console.log(Number.isInteger("Andrej"))
-console.log(Number.isInteger(42))
-console.log(Number.isInteger(42.0))
-console.log(Number.isInteger(true))
-
-console.log(Number.parseFloat("12.346").toFixed(2))
-console.log(typeof Number.parseFloat("12.346"))
-console.log(typeof Number.parseFloat("12.346").toFixed(2))
-console.log(isNaN(Number.parseFloat("12.346")))
-console.log(isNaN(Number.parseFloat("abc")))
-console.log(Number.isNaN(Number.parseFloat("abc")))
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI))
+console.log(Math.round(3.5))
+console.log(Math.ceil(3.5))
+console.log(Math.floor(3.5))
+console.log(Math.pow(2, 3))
+console.log(Math.min(6, 8, 1.4, 5))
+console.log(Math.max(6, 18, 1, 5))
+console.log(1 + Math.floor(Math.random() * 10))
+console.log(1 + Math.floor(Math.random() * 10))
+console.log(1 + Math.floor(Math.random() * 10))
+console.log(1 + Math.floor(Math.random() * 10))
+console.log(1 + Math.floor(Math.random() * 10))
+console.log(1 + Math.floor(Math.random() * 10))
