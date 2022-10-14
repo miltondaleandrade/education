ee2d18572ad96a02f97c3a6d11af547da9665887
miltondalelandrade <robertnbritton@gmail.com>
2022-10-14 09:07:10 +0200

    Lesson #4.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,23 +1,29 @@
-const myVariable = "Mathematics"
+const myNumber = 42
 
-console.log(myVariable.length)
+console.log(myNumber)
 
-console.log(myVariable.charAt(6))
+const myFloat = 42.0
 
-console.log(myVariable.indexOf("ati"))
+const myString = "42"
 
-console.log(myVariable.lastIndexOf("at"))
+console.log(myFloat)
 
-console.log(myVariable.slice(5, 8))
+console.log(myFloat === myNumber)
+console.log(myString === myNumber)
+console.log(myString === myFloat)
+console.log(Number(myString) === myFloat)
+console.log(Number(myString) === myNumber)
+console.log(Number("Andrej"))
+console.log(Number(true))
+console.log(Number(false))
+console.log(Number.isInteger("Andrej"))
+console.log(Number.isInteger(42))
+console.log(Number.isInteger(42.0))
+console.log(Number.isInteger(true))
 
-console.log(myVariable.slice(5))
-
-console.log(myVariable.toUpperCase())
-
-console.log(myVariable.toLowerCase())
-
-console.log(myVariable.includes("ema"))
-
-console.log(myVariable.includes("toma"))
-
-console.log("one,two,three,four".split(","))
+console.log(Number.parseFloat("12.346").toFixed(2))
+console.log(typeof Number.parseFloat("12.346"))
+console.log(typeof Number.parseFloat("12.346").toFixed(2))
+console.log(isNaN(Number.parseFloat("12.346")))
+console.log(isNaN(Number.parseFloat("abc")))
+console.log(Number.isNaN(Number.parseFloat("abc")))
