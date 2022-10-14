0a134b1c5c1781b24550cd07f1795423b529d05d
miltondalelandrade <robertnbritton@gmail.com>
2022-10-14 08:54:51 +0200

    Lesson #3.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,4 +1,23 @@
-console.log("Hello World!!!")
-console.log(typeof "Andrej")
-console.log(typeof 42)
-console.log(typeof true)
+const myVariable = "Mathematics"
+
+console.log(myVariable.length)
+
+console.log(myVariable.charAt(6))
+
+console.log(myVariable.indexOf("ati"))
+
+console.log(myVariable.lastIndexOf("at"))
+
+console.log(myVariable.slice(5, 8))
+
+console.log(myVariable.slice(5))
+
+console.log(myVariable.toUpperCase())
+
+console.log(myVariable.toLowerCase())
+
+console.log(myVariable.includes("ema"))
+
+console.log(myVariable.includes("toma"))
+
+console.log("one,two,three,four".split(","))
