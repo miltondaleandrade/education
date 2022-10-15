d8f1c9c75799e47d5cb40627471df358b9fc157b
miltondalelandrade <robertnbritton@gmail.com>
2022-10-15 09:11:21 +0200

    Lesson #7. Part 1.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,10 +1,15 @@
-const name = "Andrej"
-
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
-console.log(name.charAt(Math.floor(Math.random() * name.length)))
+// let soup = "chicken noodle soup";
+let customerIsBanned = false;
+let soup = "checken noodle soup";
+let reply;
+let crackers = true
+if (customerIsBanned) {
+    reply = "No soup for you!"
+} else if (soup && crackers) {
+    reply = `Here is your order of ${soup} and crackers`
+} else if (soup) {
+    reply = `Here is your order of ${soup}`
+} else {
+    reply = `Sorry, we're out of soup.`
+}
+console.log(reply)
