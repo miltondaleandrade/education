b3bd7f49d0758ea89dbdfdeacc22fc8b6d065c9a
miltondalelandrade <robertnbritton@gmail.com>
2022-10-16 12:07:33 +0200

    Lesson #9.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,28 +1,7 @@
-let playerOne = "scissors"
-let computer = "rock"
+let soup = "Chicken noodle soup";
 
-switch (playerOne) {
-    case computer:
-        console.log("Tie game!");
-        break;
-    case "rock":
-        if (computer === "paper") {
-            console.log("Computer wins!");
-        } else {
-            console.log("Player one wins");
-        }
-        break;
-    case "paper":
-        if (computer === "scissors") {
-            console.log("Computer wins!");
-        } else {
-            console.log("Player one wins");
-        }
-        break;
-    default:
-        if (computer === "rock") {
-            console.log("Computer wins!");
-        } else {
-            console.log("Player one wins");
-        }
-}
+let isCustomerBanned = true;
+
+let soupAccess = isCustomerBanned ? "Sorry, no soup for you!" : soup ? "Yes, we have soup" : "Sorry, no soup today!"
+
+console.log(soupAccess);
