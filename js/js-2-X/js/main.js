e06dec3282cd73fad0f268df2959bfa6ee15a2fa
miltondalelandrade <robertnbritton@gmail.com>
2022-11-08 18:46:42 +0100

    Lesson #21.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,30 +1,3 @@
-const makeError = () => {
-    let i = 1;
-    while (i <= 5) {
-        try {
-            // throw new Error("Custom Error!")
-            if (i % 2 !== 0) {
-                throw new MyError("Odd number!")
-            }
-            console.log("Even number!");
-        } catch (e) {
-            // console.log(e.name);
-            // console.log(e.message);
-            // console.log(e.stack);
-            console.warn(e);
-        } finally {
-            console.log("... finally");
-            i++;
-        }
-    }
-}
-
-class MyError {
-    constructor(msg) {
-        this.name = this.constructor.name;
-        this.message = msg;
-        this.stack = `${this.name}: ${msg}`;
-    }
-}
-
-makeError();
+console.log(document);
+const views = document.querySelectorAll(".myview");
+console.log(views);
