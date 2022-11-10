4d95ed8ef6abe228376c4ac295e0c08dc3e8ec64
miltondalelandrade <robertnbritton@gmail.com>
2022-11-10 16:25:31 +0100

    Lesson #22.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,3 +1,29 @@
-console.log(document);
-const views = document.querySelectorAll(".myview");
-console.log(views);
+document.addEventListener("readystatechange", ev => {
+    if (document.readyState === "complete") {
+        init();
+    }
+});
+
+function init() {
+    const button = document.querySelector(".myButton");
+    button.addEventListener("mouseover", evt => {
+       evt.target.classList.add("hover");
+       evt.target.classList.remove("norm");
+       evt.target.classList.remove("clicked");
+    });
+    button.addEventListener("mouseout", evt => {
+       evt.target.classList.remove("hover");
+       evt.target.classList.remove("clicked");
+       evt.target.classList.add("norm");
+    });
+    button.addEventListener("mousedown", evt => {
+       evt.target.classList.remove("hover");
+       evt.target.classList.remove("norm");
+       evt.target.classList.add("clicked");
+    });
+    button.addEventListener("mouseup", evt => {
+        evt.target.classList.add("hover");
+        evt.target.classList.remove("norm");
+        evt.target.classList.remove("clicked");
+    });
+}
