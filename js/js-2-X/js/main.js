26afaa91fef96677c6b880db938cc7c30bd944a6
miltondalelandrade <robertnbritton@gmail.com>
2022-11-14 18:32:00 +0100

    Lesson #26.1.

--- js/js-2-X/js/main.js
+++ js/js-2-X/js/main.js
@@ -1,33 +1,15 @@
-import Person from "./module";
+const users = fetch("https://jsonplaceholder.typicode.com/users");
 
-document.addEventListener("readystatechange", ev => {
-    if (document.readyState === "complete") {
-        const andrej = new Person("Andrej", "Istomin");
-        console.log(andrej.greeting());
-    }
-});
+// pending
+console.log(users);
 
-
-function buttons() {
-    const button = document.querySelector(".myButton");
-    button.addEventListener("mouseover", evt => {
-       evt.target.classList.add("hover");
-       evt.target.classList.remove("norm");
-       evt.target.classList.remove("clicked");
-    });
-    button.addEventListener("mouseout", evt => {
-       evt.target.classList.remove("hover");
-       evt.target.classList.remove("clicked");
-       evt.target.classList.add("norm");
-    });
-    button.addEventListener("mousedown", evt => {
-       evt.target.classList.remove("hover");
-       evt.target.classList.remove("norm");
-       evt.target.classList.add("clicked");
-    });
-    button.addEventListener("mouseup", evt => {
-        evt.target.classList.add("hover");
-        evt.target.classList.remove("norm");
-        evt.target.classList.remove("clicked");
-    });
-}
+// properly:
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(resp => {
+        return resp.json();
+    })
+    .then(json => {
+        json.forEach(user => {
+            console.log(user.name);
+        })
+    })
