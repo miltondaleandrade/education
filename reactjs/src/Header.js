ee60c307f8ab9365a31ae18a850faebf158b6167
miltondalelandrade <robertnbritton@gmail.com>
2022-11-21 13:17:21 +0100

    Lesson #4.1.

--- reactjs/src/Header.js
+++ reactjs/src/Header.js
@@ -1,6 +1,11 @@
 const Header = () => {
+    const headerStyle = {
+        backgroundColor: 'mediumblue',
+        color: '#fff'
+    };
+
     return (
-        <header>
+        <header style={headerStyle}>
             <h1>Groceries List</h1>
         </header>
     )
