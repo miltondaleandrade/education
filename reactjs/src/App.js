b45550ad9de0d63df8fd3f64f8acdd34173b740c
miltondalelandrade <robertnbritton@gmail.com>
2022-12-06 09:31:41 +0100

    Lesson #9.2.

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -23,23 +23,49 @@ function App() {
         }
     ]);
 
+    const [newItem, setNewItem] = useState('')
+
+    const setAndSaveItems = (newItems) => {
+        setItems(newItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+    }
+
+    const addItem = (item) => {
+        console.log("Add: " + item);
+        const id = items.length ? items[items.length - 1] : 1;
+        const myNewItem = {id: id, checked: false, name: item};
+        const listItems = [...items, myNewItem];
+        setAndSaveItems(listItems);
+    }
+
     const handleClick = (id) => {
         const listItems = items.map(
             (item) =>
                 item.id === id ? {...item, checked: !item.checked} : item
         );
-        setItems(listItems);
+        setAndSaveItems(listItems);
     }
 
     const handleDelete = (id) => {
         const listItems = items.filter(item => item.id !== id)
-        setItems(listItems);
+        setAndSaveItems(listItems);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!newItem) return;
+        addItem(newItem);
+        setNewItem('');
     }
 
     return (
         <div className="App">
             <Header title="Grocery List"/>
-            <ItemAdd/>
+            <ItemAdd
+                newItem={newItem}
+                setNewItem={setNewItem}
+                handleSubmit={handleSubmit}
+            />
             <Content
                 items={items}
                 handleClick={handleClick}
--- reactjs/src/ItemAdd.js
+++ reactjs/src/ItemAdd.js
@@ -1,21 +1,23 @@
 import {FaPlus} from "react-icons/fa";
 
-const ItemAdd = () => {
+const ItemAdd = ({newItem, setNewItem, handleSubmit}) => {
     return (
-       <form className="addForm">
-           <label htmlFor="addItem">Add Item</label>
-           <input
-               autoFocus
-               id="addItem"
-               type="text"
-               placeholder="Add Item"
-               required
-           />
-           <button
-               type="submit"
-               aria-label="Add Item"
-           ><FaPlus/></button>
-       </form>
+        <form className="addForm" onSubmit={handleSubmit}>
+            <label htmlFor="addItem">Add Item</label>
+            <input
+                autoFocus
+                id="addItem"
+                type="text"
+                placeholder="Add Item"
+                required
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+            />
+            <button
+                type="submit"
+                aria-label="Add Item"
+            ><FaPlus/></button>
+        </form>
     )
 }
 
