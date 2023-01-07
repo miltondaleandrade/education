236e93d8fbfedf1ea5415266891e31321194e70c
miltondalelandrade <robertnbritton@gmail.com>
2023-01-07 09:40:26 +0100

    Colour picker app. Improved implementation with the colour picker component.

--- vitejs/colour-picker-challenge/src/App.jsx
+++ vitejs/colour-picker-challenge/src/App.jsx
@@ -2,15 +2,25 @@ import './App.css'
 import Square from "./Square.jsx";
 import Input from "./Input.jsx";
 import {useState} from "react";
+import {SketchPicker} from "react-color";
 
 function App() {
 
-    const [colorValue, setColorValue] = useState('')
+    const [colorValue, setColorValue] = useState('#000')
+
+    const onPickerComplete = (color) => {
+        console.log(color)
+        setColorValue(color.hex)
+    }
 
     return (
         <div className="App">
             <Square colorValue={colorValue}/>
             <Input colorValue={colorValue} setColorValue={setColorValue}/>
+            <SketchPicker
+                color={ colorValue }
+                onChangeComplete={ onPickerComplete }
+            />
         </div>
     )
 }
