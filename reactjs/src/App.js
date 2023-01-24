bfb381298992ead87a11a3f17f22e646576f1154
miltondalelandrade <robertnbritton@gmail.com>
2023-01-24 12:20:00 +0100

    Lesson #14.1. Add new item.

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import {useState, useEffect} from "react";
 import ItemAdd from "./ItemAdd";
 import ItemSearch from "./ItemSearch";
+import apiRequest from "./apiRequest";
 
 function App() {
 
@@ -42,11 +43,22 @@ function App() {
         }, 10000);
     }, [])
 
-    const addItem = (item) => {
+    const addItem = async (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
         const myNewItem = {id, checked: false, item};
         const listItems = [...items, myNewItem];
         setItems(listItems);
+        const postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(myNewItem)
+        }
+        const result = await apiRequest(API_URL, postOptions);
+        if (result) {
+            setFetchError(result)
+        }
     }
 
     const handleCheck = (id) => {
