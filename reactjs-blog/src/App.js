572ccf52f8eaf8522ff3916cac0e9c68e6167894
miltondalelandrade <robertnbritton@gmail.com>
2023-01-26 12:04:26 +0100

    Lesson 16.3. Simple routing is implemented.

--- reactjs-blog/src/App.js
+++ reactjs-blog/src/App.js
@@ -1,6 +1,27 @@
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import {Route, Routes, useHistory} from 'react-router-dom';
+import {useState, useEffect} from "react";
+
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Nav/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route exact path="/post" element={<NewPost/>}/>
+            <Route exact path="/post/:id" element={<PostPage/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="*" element={<Missing/>}/>
+        </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ reactjs-blog/src/Missing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Missing = () => {
+    return (
+        <main>
+            <h1>Error: The page is not found.</h1>
+        </main>
+    );
+};
+
+export default Missing;
