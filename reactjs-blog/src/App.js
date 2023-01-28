845ade7818cf4c92a5d36a6621bb256976365a97
miltondalelandrade <robertnbritton@gmail.com>
2023-01-28 11:03:25 +0100

    Lesson 17.3. New Post feature.

--- reactjs-blog/src/App.js
+++ reactjs-blog/src/App.js
@@ -8,6 +8,7 @@ import About from './About';
 import Missing from './Missing';
 import {Route, Routes, useNavigate} from 'react-router-dom';
 import {useState, useEffect} from "react";
+import {format} from "date-fns";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -32,20 +33,48 @@ function App() {
     ]);
     const [search, setSearch] = useState('');
     const [searchResults, setSearchResults] = useState([]);
+    const [postTitle, setPostTitle] = useState('');
+    const [postBody, setPostBody] = useState('');
     const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+        const dateTime = format(new Date(), 'MMMM dd, yyyy pp');
+        const newPost = {
+            id: id,
+            dateTime: dateTime,
+            title: postTitle,
+            body: postBody
+        };
+        const allPosts = [...posts, newPost];
+        setPosts(allPosts);
+        setPostTitle('');
+        setPostBody('');
+        navigate('/');
+    }
 
     const handleDelete = (id) => {
         const postsList = posts.filter(post => post.id !== id);
         setPosts(postsList);
         navigate('/');
-    };
+    }
+
     return (
         <div className="App">
             <Header title="React JS Blog"/>
             <Nav search={search} setSearch={setSearch}/>
             <Routes>
                 <Route path="/" element={<Home posts={posts}/>}/>
-                <Route exact path="/post" element={<NewPost/>}/>
+                <Route exact path="/post"
+                       element={<NewPost
+                           handleSubmit={handleSubmit}
+                           postTitle={postTitle}
+                           setPostTitle={setPostTitle}
+                           postBody={postBody}
+                           setPostBody={setPostBody}
+                       />}
+                />
                 <Route exact path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete}/>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="*" element={<Missing/>}/>
