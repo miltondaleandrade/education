ee4195a11221304086c35843aafb7118f9bbb420
miltondalelandrade <robertnbritton@gmail.com>
2023-01-31 17:35:55 +0100

    Lesson 19.5. Edit post with axios.

--- reactjs-blog/src/App.js
+++ reactjs-blog/src/App.js
@@ -10,6 +10,7 @@ import {Route, Routes, useNavigate} from 'react-router-dom';
 import {useState, useEffect} from "react";
 import {format} from "date-fns";
 import api from './api/posts';
+import EditPost from "./EditPost";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -17,6 +18,8 @@ function App() {
     const [searchResults, setSearchResults] = useState([]);
     const [postTitle, setPostTitle] = useState('');
     const [postBody, setPostBody] = useState('');
+    const [editTitle, setEditTitle] = useState('');
+    const [editBody, setEditBody] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -61,6 +64,25 @@ function App() {
         }
     }
 
+    const handleEdit = async (id) => {
+        const dateTime = format(new Date(), 'MMMM dd, yyyy pp');
+        const updatedPost = {
+            id: id,
+            dateTime: dateTime,
+            title: editTitle,
+            body: editBody
+        };
+        try {
+            const reponse = await api.put(`/posts/${id}`, updatedPost);
+            setPosts(posts.map(post => post.id === id ? {...reponse.data} : post));
+            setEditTitle('');
+            setEditBody('');
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const handleDelete = async (id) => {
         try {
             await api.delete(`posts/${id}`);
@@ -85,6 +107,16 @@ function App() {
                            setPostTitle={setPostTitle}
                            postBody={postBody}
                            setPostBody={setPostBody}
+                       />}
+                />
+                <Route path="/edit/:id"
+                       element={<EditPost
+                           posts={posts}
+                           handleEdit={handleEdit}
+                           editTitle={editTitle}
+                           setEditTitle={setEditTitle}
+                           editBody={editBody}
+                           setEditBody={setEditBody}
                        />}
                 />
                 <Route exact path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete}/>}/>
